30b810b9a52167fd13c0abc4d6d6359188ac5c26
Jayson <frawinkin06@gmail.com>
2023-02-22 20:38:31 -0400

    Se le agrego la cadena de conexion a la BD.

--- Xtasis/Xtasis/Clases/ConexionBd.cs
+++ Xtasis/Xtasis/Clases/ConexionBd.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Xtasis.Clases
 {
     class ConexionBd
     {
+        public static string cadena = "Data Source=example.mssql.somee.com;Initial Catalog=example;User ID=grosario_SQLLogin_1;Password=******";
     }
 }
